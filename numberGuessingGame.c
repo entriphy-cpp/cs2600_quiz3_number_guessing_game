3db4b4a4562df4a772c2f58a779bc47c6f8e7a9b
Matthew Finerty <mfinerty@cpp.edu>
2022-10-23 15:51:33 -0700

    Create intial code outline

---
+++ numberGuessingGame.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int displayMenu(); // Display main menu
+int getMaxNumber(); // Gets max number from user
+void guessingGame(int maxNumber); // Main game loop
+
+int main(void) {
+    // Display menu (implement as function)
+    //  Press 1 to play a game
+    //  Press 2 to change the max number
+    //  Press 3 to quit
+
+    // Option 1: Play game
+
+    // Option 2: Change max number
+
+    // Option 3: Quit
+}
+
+int displayMenu() {
+    // Display menu
+
+    // Read user input
+
+    // Check if input is valid (falls in range 1-3)
+
+    // Return option specified by user
+    
+    return 0; // Placeholder return
+}
+
+int getMaxNumber() {
+    // Read user input
+
+    // Check if input is valid (is not negative)
+
+    // Return max number
+
+    return 0; // Placeholder return
+}
+
+void guessingGame(int maxNumber) {
+    // Generate random number between 1 and maxNumber
+
+    // Loop:
+
+    //  Get number from user
+
+    //  Exit program if user entered 'q' (return -1)
+
+    //  Exit loop if number equals random number
+
+    //  Print higher/lower if number does not equal random number
+}
